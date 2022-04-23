6f4ae1264410b31e45f40975a699a1c3924c610e
adam7400 <adam.berdis@hotmail.com>
2022-04-23 10:09:50 +0100

    006: added some skeleton to DG (no Random functionality)

--- src/main/java/com/m3/basics/DogGenetics.java
+++ src/main/java/com/m3/basics/DogGenetics.java
@@ -1,4 +1,20 @@
 package com.m3.basics;
 
+import java.util.Scanner;
+
 public class DogGenetics {
-}
+    public static void main(String[] args) {
+        Scanner yourInput = new Scanner(System.in);
+
+        System.out.println("What is your dog's name?");
+        String name = yourInput.next();
+        generateReport(name);
+
+    }
+
+    public static void generateReport(String name){
+        String[] breeds = {"German Shepherd", "Husky", "Rottweiler", "Bulldog", "Corgi"};
+        System.out.println(name + " is:");
+
+        }
+    }
